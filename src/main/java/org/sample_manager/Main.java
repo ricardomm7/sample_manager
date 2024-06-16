d243d231d569b3bac7476463d9f846e49fc25c8b
Ricardo Meireles <ricardomanuel.meireles@gmail.com>
2024-06-16 23:36:08 +0100

    Added a favicon to the GUI. #9

--- src/main/java/org/sample_manager/Main.java
+++ src/main/java/org/sample_manager/Main.java
@@ -3,9 +3,11 @@ package org.sample_manager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
     @Override
@@ -14,6 +16,7 @@ public class Main extends Application {
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Sample Manager - v0.1 (Beta)");
         stage.setScene(scene);
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("icon.png"))));
         stage.show();
     }
 
