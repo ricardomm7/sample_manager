9ae57a235c7eb25e1933ddc0ab266d0cb54c2e41
Ricardo Meireles <ricardomanuel.meireles@gmail.com>
2024-06-30 21:24:41 +0100

    Hazard enum class created. #21

---
+++ src/main/java/org/sample_manager/Domain/HazardTypes.java
@@ -0,0 +1,44 @@
+package org.sample_manager.Domain;
+
+public enum HazardTypes {
+    CHEMICAL {
+        @Override
+        public String toString() {
+            return "Chemical Hazard";
+        }
+    },
+    PHYSICAL {
+        @Override
+        public String toString() {
+            return "Physical Hazard";
+        }
+    },
+    BIOLOGICAL {
+        @Override
+        public String toString() {
+            return "Biological Hazard";
+        }
+    },
+    ERGONOMIC {
+        @Override
+        public String toString() {
+            return "Ergonomic Hazard";
+        }
+    },
+    PSYCHOSOCIAL {
+        @Override
+        public String toString() {
+            return "Psychosocial Hazard";
+        }
+    },
+    RADIATION {
+        @Override
+        public String toString() {
+            return "Radiation Hazard";
+        }
+    };
+
+    @Override
+    public abstract String toString();
+}
+
